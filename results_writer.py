4ac5092f23f59523a45b6bf292287f3c5d2dca7f
William Jones <wisaacj@live.co.uk>
2024-09-13 14:08:06 +0100

    style: using pathlib instead of os.path

--- results_writer.py
+++ results_writer.py
@@ -1,9 +1,10 @@
 import os
 import pandas as pd
 
+from pathlib import Path
 from model import CLO, Snapshot
 
-OUTPUT_DIR = "./outputs"
+OUTPUT_DIR = Path("outputs")
 
 
 class ResultsWriter:
@@ -12,24 +13,22 @@ class ResultsWriter:
         self, 
         model: CLO, 
         deal_id: str,
-        output_dir: str = OUTPUT_DIR
+        output_dir: Path = OUTPUT_DIR
     ):
         self.model = model
         self.deal_id = deal_id
         self.output_dir = output_dir
-
-        if not os.path.exists(output_dir):
-            os.mkdir(output_dir)
+        output_dir.mkdir(exist_ok=True)
 
     @property
-    def output_path(self) -> str:
-        return os.path.join(self.output_dir, self.deal_id)
+    def output_path(self) -> Path:
+        return self.output_dir / self.deal_id
 
     def write_results(self) -> str:
         for tranche in self.model.tranches:
             self._export_history_to_excel(tranche.history, tranche.rating)
 
-        return os.path.abspath(self.output_path)
+        return self.output_dir.absolute()
 
     def _export_history_to_excel(self, snapshots: list[Snapshot], filename: str):
         """Exports a list of snapshot objects to an Excel file."""
@@ -37,10 +36,8 @@ class ResultsWriter:
         data = [ResultsWriter._snapshot_to_dict(snapshot) for snapshot in snapshots]
         cashflows_df = pd.DataFrame(data)
         
-        if not os.path.exists(self.output_path):
-            os.mkdir(self.output_path)
-
-        cashflows_df.to_csv(os.path.join(self.output_path, f"{filename}.csv"))
+        self.output_path.mkdir(exist_ok=True)
+        cashflows_df.to_excel(self.output_path / f"{filename}.xlsx")
 
     @staticmethod
     def _snapshot_to_dict(snapshot: Snapshot):
