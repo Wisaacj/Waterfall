ea5559145a8f6d646d37d0abbda2f01334d3addc
William Jones <wisaacj@live.co.uk>
2024-10-01 17:40:05 +0100

    chore: re-export enums

--- model/__init__.py
+++ model/__init__.py
@@ -1,5 +1,5 @@
 from .account import Account
-from .asset import Asset, Loan, Bond
+from .asset import Asset
 from .fee import Fee, IncentiveFee
 from .collateralised_loan_obligation import CLO
 from .enums import WaterfallItem
@@ -9,3 +9,4 @@ from .portfolio import Portfolio
 from .waterfall import CashflowWaterfall
 from .snapshots import Snapshot
 from .forward_rate_curve import ForwardRateCurve
+from .enums import *
